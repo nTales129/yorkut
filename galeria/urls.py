4cd524abe8c3a4223e54641c86e41a48a3c54e62
NTales <128847014+nTales129@users.noreply.github.com>
2024-06-14 18:04:25 -0300

    -a

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from galeria.views import index
+from galeria.views import index, cadastro 
 
 urlpatterns = [
     path('', index),
+    path('cadastro/', cadastro, name='cadastro')
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 def  index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
+
+def cadastro(request):
+    return render(request, 'galeria/cadastro.html')
     
 
 # Create your views here.
